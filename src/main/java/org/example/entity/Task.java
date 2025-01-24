e0c8aa0cf8c94cbd81da9e0c34edce3874edcd18
Ferhat Genç <ferhatgenc12@gmail.com>
2025-01-24 17:16:25 +0300

    basic classes have been created

---
+++ src/main/java/org/example/entity/Task.java
@@ -0,0 +1,24 @@
+package org.example.entity;
+
+import java.util.Objects;
+
+public class Task {
+    private String project;
+    private String description;
+    private String assignee;
+    private Priority priority;
+    private Status status;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Task task = (Task) o;
+        return Objects.equals(project, task.project) && Objects.equals(description, task.description);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(project, description);
+    }
+}
---
+++ src/main/java/org/example/entity/TaskData.java
@@ -0,0 +1,23 @@
+package org.example.entity;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class TaskData {
+    Set<Task> annsTasks = new HashSet<>();
+    Set<Task> bobsTasks = new HashSet<>();
+    Set<Task> carolsTasks = new HashSet<>();
+    Set<Task> unassignedTasks = new HashSet<>();
+
+    public TaskData(Set<Task> annsTasks, Set<Task> bobsTasks, Set<Task> carolsTasks, Set<Task> unassignedTasks) {
+        this.annsTasks = annsTasks;
+        this.bobsTasks = bobsTasks;
+        this.carolsTasks = carolsTasks;
+        this.unassignedTasks = unassignedTasks;
+    }
+
+    public Set<Task> getTasks(String str) {
+
+
+    }
+}
